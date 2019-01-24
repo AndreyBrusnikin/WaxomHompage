0c3c504b9f8caab9c2b54052081abfcaae5bafbb
AndreyBrusnikin <andreybrusnikin@gmail.com>
2019-01-24 23:51:56 +0200

    commit

--- dist/js/collapse.js
+++ dist/js/collapse.js
@@ -1,13 +1,13 @@
-(function() {
-  let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
-let iconToggle = document.getElementById("icon-toggle-menu");
-let iconToggleClose = document.getElementById("icon-toggle-close");
+(window.onload=function() {
+    let mainNav = document.getElementById("js-menu");
+    let navBarToggle = document.getElementById("js-navbar-toggle");
+    let iconToggle = document.getElementById("icon-toggle-menu");
+    let iconToggleClose = document.getElementById("icon-toggle-close");
 
-navBarToggle.addEventListener("click", function() {
-    mainNav.classList.toggle("active");
-    iconToggle.classList.toggle("noactive");
-    iconToggleClose.classList.toggle("active");
-});
+    navBarToggle.addEventListener("click", function() {
+        mainNav.classList.toggle("active");
+        iconToggle.classList.toggle("noactive");
+        iconToggleClose.classList.toggle("active");
+    });
 
 }());
